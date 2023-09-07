0eef16287f1bd805bbf1f6061ec4b201a4ab5a30
GSinghh <Gbassans@yahoo.com>
2023-09-07 13:43:02 -0700

    Finished Etch-A-Sketch

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-const RGBMode = document.getElementById('RGB');
+const RGBMode = document.getElementById('rgb');
 const numSlider = document.getElementById('range-selector');
 const eraserMode = document.getElementById('eraser');
-const colorSelecter = document.getElementById('color-selector');
 const divContainer = document.getElementById('div-container');
 const gridNumber = document.getElementById('grid-number');
+const black = document.getElementById('black');
+
+let mode;
 
 numSlider.addEventListener("input", addDivs);
-eraserMode.addEventListener("click",getColor);
+eraserMode.addEventListener("click",getMode);
+RGBMode.addEventListener("click", getMode);
+black.addEventListener("click", getMode);
+
 
 function addDivs()
 {
@@ -33,12 +38,31 @@ function getColor(event)
     /* Depending on the object that calls this function, 
     we will know whether we're changing the color to the color from the selector
     RGB or the eraser */
-    console.log("Teri Maa Di Fudi");
+
+    let color;
+
+    if (mode == "rgb")
+    {
+        let r = Math.floor(Math.random() * 256);
+        let g = Math.floor(Math.random() * 256);
+        let b = Math.floor(Math.random() * 256);
+        color = `rgb(${r},${g},${b}`;
+    }
+    else if(mode == "eraser")
+    {
+        color = 'white';
+    }
+    else if(mode == "black")
+    {
+        color = 'black'
+    }
+
+    event.target.style.backgroundColor = color;
 }
 
-function eraser()
+function getMode(event)
 {
-
+    mode = event.target.id;
 }
 
 addDivs(); //16 x 16 grid is initialized as soon as page is loaded
